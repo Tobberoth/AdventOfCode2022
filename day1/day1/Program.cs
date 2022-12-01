3386269ff60daa38727fec16cde75762a126d7bd
Tobias Dersén <tobberoth@gmail.com>
2022-12-01 10:07:06 +0100

    Day 1 finished

---
+++ day1/day1/Program.cs
@@ -0,0 +1,19 @@
+﻿string inputFile = "input.txt";
+List<int> elfCalories = new();
+int count = 0;
+foreach (var line in File.ReadAllLines(inputFile))
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        elfCalories.Add(count);
+        count = 0;
+    }
+    else
+        count += int.Parse(line);
+}
+elfCalories.Sort();
+elfCalories.Reverse();
+// Step 1
+Console.WriteLine(elfCalories[0]);
+// Step 2
+Console.WriteLine(elfCalories.Take(3).Sum());
